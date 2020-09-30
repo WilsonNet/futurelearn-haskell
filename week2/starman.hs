1e9a4eb09445ca30f35e0c3c79d4b6ae1fa727d4
Wilson Neto <wilsonpsn@hotmail.com>
2020-09-30 09:53:26 -0300

    Added turn function

--- week2/starman.hs
+++ week2/starman.hs
@@ -5,3 +5,10 @@ check word display c
             then c
             else y | (x, y) <- zip word display])
 
+turn :: String -> String -> Int-> IO ()
+turn word display n =
+  do if n == 0
+       then putStrLn "You Lose"
+       else if word == display
+              then putStrLn "You Win!"
+              else mkguess word display n
