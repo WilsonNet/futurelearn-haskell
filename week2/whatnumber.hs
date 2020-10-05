f7cf42f99d0efda1071d0dc79fdebf52f5dbe0dc
Wilson Neto <wilsonpsn@hotmail.com>
2020-10-05 09:47:10 -0300

    Advanced a little bit

--- week2/whatnumber.hs
+++ week2/whatnumber.hs
@@ -6,4 +6,24 @@ check number guess
                then (False, "Guess Higher")
                else (False, "Guess Lower")
 
-              
+
+turn :: Int -> Int -> Int -> IO ()
+turn number n
+  do if n == 0 
+    then putStrnLn "You Lose"
+    else
+      if (head checkResult == True)
+        putStrLn tip
+      else 
+        mkguess n tip
+
+mkguess :: Int -> IO()
+mkguess tip n = 
+    do putStrlLn (tip)
+       putStrLn "Enter your guess: "
+       q <- getLine
+       let (win, tip) = check number guess q
+
+      
+whatnumber :: Int -> Int -> IO ()              
+whatnumber number n = turn number n
