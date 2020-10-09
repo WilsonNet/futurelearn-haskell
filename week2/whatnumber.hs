159471c72180358b2d1248ace9a8118e84711d2f
Wilson Neto <wilsonpsn@hotmail.com>
2020-10-09 10:01:52 -0300

    whatnumber is working

--- week2/whatnumber.hs
+++ week2/whatnumber.hs
@@ -1,4 +1,4 @@
-check :: Int -> Int -> Bool
+check :: Int -> Int -> (Bool, String)
 check number guess
      = if number == guess 
           then (True, "You Win")
@@ -6,26 +6,24 @@ check number guess
                then (False, "Guess Higher")
                else (False, "Guess Lower")
 
-
 turn :: Int -> Int -> Bool -> String -> IO ()
-turn number n status tip
+turn number n status tip =
   do if n == 0 
-    then putStrLn "You Lose"
-    else if (status == True)
-         putStrLn tip 
-      else 
-         putStrLn tip
-         mkguess number
+       then putStrLn "You Lose"
+       else if (status == True)
+               then  putStrLn tip 
+               else do putStrLn tip
+                       mkguess number n
 
-mkguess :: Int -> IO()
+mkguess :: Int -> Int -> IO()
 mkguess number n = 
-  do putStrLn "Enter your guess: "
-     q <- getLine
-     let (status, tip) = check number guess q
+  do putStrLn ("You have " ++ show n ++ " tries")
+     putStrLn "Enter your guess: "
+     q  <- getLine
+     let q' = (read q :: Int)
+     let (status, tip) = check number q'
      let n' = if status then n else n-1
-     turn n' status tip
-
-
+     turn number n' status tip
       
 whatnumber :: Int -> Int -> IO ()              
 whatnumber number n = turn number n False "Make your first guess"
