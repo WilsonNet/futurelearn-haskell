bd988479f8c74db097f5231e3cd5514f5f845daf
Wilson Neto <wilsonpsn@hotmail.com>
2020-11-28 17:39:13 -0300

    Speller is now working

--- week3/speller.hs
+++ week3/speller.hs
@@ -1,22 +1,30 @@
-test :: [Char] -> [Char]
-test word = word
-
 getFirstLetter :: [Char] -> [Char]
-getFirstLetter word = 
-  let
-    firstLetter = head word
-  in
-    firstLetter : " is for " ++  word
-
+getFirstLetter "" = ""
+getFirstLetter word =
+  let firstLetter = head word
+   in firstLetter : " is for " ++ word
 
 speller :: [[Char]] -> [Char]
-speller [] = "" 
--- speller wordList = foldl function "" wordList
+speller [] = []
+speller list =
+  let   
+    mapped = map getFirstLetter list
+    resulted = foldl (\acc elt -> acc ++ ", " ++ elt) "" mapped
+  in
+    resulted
+
+test1 = speller ["abacus"]
+test2 = speller ["whisky", "x-ray"]
+test3 = speller ["whisky", "x-ray", "", "bar"]
+
+main :: IO ()
+main =
+  do
+    print test1
+    print test2
+    print test3
 
 -- Tests
--- speller ["abacus"] -- > "a is for abacus"
 -- speller [] -- > ""
--- speller ["apple", "banana", "coconut"] 
- -- > "a is for apple, b is for banana, and c is for coconut"
--- speller ["whisky", "x-ray"]
- -- > "w is for whisky, and x is for x-ray"
+-- > "a is for apple, b is for banana, and c is for coconut"
+-- > "w is for whisky, and x is for x-ray"
